ffc263c3308223b00cabd0397b3236f676ddd8cc
MaksimGayduk <maksim.gayduk@gmail.com>
2018-11-09 18:10:46 +0200

    Created hostInfo method

--- src/modules/HostModule.php
+++ src/modules/HostModule.php
@@ -3,11 +3,20 @@
 namespace hiapi\heppy\modules;
 
 
+use err;
+
 class HostModule extends AbstractModule
 {
     public function hostSet(array $row): array
     {
-        $data = $this->hostCreate($row);
+        $info = $this->hostInfo($row);
+
+        if (err::is($info) || empty($info['host'])) {
+            $data = $this->hostCreate($row);
+        }
+        else {
+
+        }
     }
 
     /**
@@ -31,6 +40,33 @@ class HostModule extends AbstractModule
             'created_date'      => $data['crDate'],
             'server_trid'       => $data['svTRID'],
             'client_trid'       => $data['clTRID'],
+        ]);
+    }
+
+    /**
+     * @param array $row
+     * @return array
+     */
+    public function hostInfo(array $row): array
+    {
+        $data = $this->tool->request([
+            'command'   => 'host:info',
+            'name'      => $row['host'],
+        ]);
+
+        return array_filter([
+            'host'              => $data['name'],
+            'ips'               => $data['ips'],
+            'result_msg'        => $data['result_msg'],
+            'result_code'       => $data['result_code'],
+            'result_lang'       => $data['result_lang'],
+            'result_reason'     => $data['result_reason'],
+            'server_trid'       => $data['svTRID'],
+            'client_trid'       => $data['clTRID'],
+            'roid'              => $data['roid'],
+            'statuses'          => implode(',', array_keys($data['statuses'])),
+            'created_by'        => $data['crID'],
+            'created_date'      => $data['crDate'],
         ]);
     }
 }
