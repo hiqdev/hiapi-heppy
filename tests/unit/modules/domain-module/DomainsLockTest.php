d2c9d47949d0e23e93cf5e219c883b0689b116e1
MaksimGayduk <maksim.gayduk@gmail.com>
2018-11-21 17:08:58 +0200

    Created 'DomainsDisableLockTest()'

--- tests/unit/modules/domain-module/DomainsLockTest.php
+++ tests/unit/modules/domain-module/DomainsLockTest.php
@@ -6,43 +6,73 @@ use hiapi\heppy\tests\unit\TestCase;
 
 class DomainsLockTest extends TestCase
 {
+    /**
+     * @var int
+     */
     private $id = 25844450;
+
+    /**
+     * @var string
+     */
     private $domain = 'silverfires1.me';
+
+    /**
+     * @return array
+     */
+    private function getApiData(): array
+    {
+        return [
+            $this->id => [
+                'domain' => $this->domain,
+                'id'     => $this->id,
+            ],
+        ];
+    }
 
     public function testDomainsEnableLock()
     {
         $tool = $this->createTool([
-            'name'    => 'silverfires1.me',
+            'name'    => $this->domain,
+            'command' => 'domain:update',
             'add'     => [
                 'statuses' => [
                     'clientDeleteProhibited'   => null,
                     'clientTransferProhibited' => null,
                 ],
             ],
-            'command' => 'domain:update',
         ], $this->getCommonSuccessResponse());
 
-        $result = $tool->domainsEnableLock([
-            $this->id => [
-                'domain' => $this->domain,
-                'id'     => $this->id,
-            ],
-        ]);
+        $result = $tool->domainsEnableLock($this->getApiData());
 
         $this->assertSame($result, [
-            25844450 => array_merge([
-                'id'          => $this->id,
-                'domain'      => $this->domain,
-            ], $this->getMappedCommonSuccessResponse()),
+            $this->id => $this->addMappedCommonSuccessResponse([
+                'id'     => $this->id,
+                'domain' => $this->domain,
+            ]),
         ]);
     }
 
-//    public function testDomainsDisableLock()
-//    {
-//        $tool = $this->createTool();
-//
-//        $result = $tool->domainsDisableLock();
-//
-//        $this->assertSame($result, );
-//    }
+    public function testDomainsDisableLock()
+    {
+        $tool = $this->createTool([
+            'name' => $this->domain,
+            'command' => 'domain:update',
+            'rem'  => [
+                'statuses' => [
+                    'clientUpdateProhibited'   => null,
+                    'clientDeleteProhibited'   => null,
+                    'clientTransferProhibited' => null,
+                ],
+            ],
+        ], $this->getCommonSuccessResponse());
+
+        $result = $tool->domainsDisableLock($this->getApiData());
+
+        $this->assertSame($result, [
+            $this->id => $this->addMappedCommonSuccessResponse([
+                'id'     => $this->id,
+                'domain' => $this->domain,
+            ]),
+        ]);
+    }
 }
