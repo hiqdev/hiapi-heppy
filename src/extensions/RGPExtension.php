c76bad4e9ee0532c6160da8698e9c6efe9ec5331
Yurii Myronchuk <bladeroot@gmail.com>
2020-01-16 21:26:40 +0000

    improve rgp extension

--- src/extensions/RGPExtension.php
+++ src/extensions/RGPExtension.php
@@ -7,17 +7,16 @@ class RGPExtension
 {
     public function apply(string $command, array $data): array
     {
-        $zone = $this->findZone($command, $data);
-        if ($zone) {
-            return $this->addNamestoreExt($data, $zone);
+        if ($command === 'domain:restore') {
+            return $this->addRGPExt($data);
         }
 
         return $data;
     }
 
-    public function isApplicable(string $command, array $data): bool
+    public function isApplicable(string $command, array $data = []): bool
     {
-        return !empty($this->findZone($command, $data));
+        return $command === 'domain:restore';
     }
 
     /**
@@ -25,37 +24,12 @@ class RGPExtension
      * @param string|null $zone
      * @return array
      */
-    public function addNamestoreExt(array $data, string $zone): array
+    public function addRGPExt(array $data = []): array
     {
-        $zone = strtoupper($zone);
-        if (in_array($zone, ['COM', 'NET', 'CC', 'TV', 'NAME'])) {
-            $extension = [
-                'command' => 'namestoreExt',
-                'subProduct' => "dot$zone",
-            ];
-            $data['extensions'][] = $extension;
-        }
+        $data['extensions'][] = [
+            'command' => 'rgp',
+        ];
 
         return $data;
-    }
-
-    /**
-     * @param array $data
-     * @param string|null $name
-     * @return null|string
-     */
-    private function findZone(string $command, array $data, string $name = null): ?string
-    {
-        if (isset($data['zone'])) {
-            return $data['zone'];
-        }
-        if (!$name && isset($data['name'])) {
-            $name = $data['name'];
-        }
-        if (!$name && isset($data['names'])) {
-            $name = reset($data['names']);
-        }
-
-        return array_pop(explode('.', $name));
     }
 }
