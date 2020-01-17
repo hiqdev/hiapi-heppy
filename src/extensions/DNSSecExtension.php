d6127e5d886d1b95e34d19342b9cadd97778863c
Yurii Myronchuk <bladeroot@gmail.com>
2020-01-17 09:36:24 +0000

    change to sceleton

--- src/extensions/DNSSecExtension.php
+++ src/extensions/DNSSecExtension.php
@@ -7,11 +7,6 @@ class DNSSecExtension
 {
     public function apply(string $command, array $data): array
     {
-        $zone = $this->findZone($command, $data);
-        if ($zone) {
-            return $this->addDNSSexExt($data, $zone);
-        }
-
         return $data;
     }
 
@@ -27,35 +22,6 @@ class DNSSecExtension
      */
     public function addDNSSecExt(array $data, string $zone): array
     {
-        $zone = strtoupper($zone);
-        if (in_array($zone, ['COM', 'NET', 'CC', 'TV', 'NAME'])) {
-            $extension = [
-                'command' => 'namestoreExt',
-                'subProduct' => "dot$zone",
-            ];
-            $data['extensions'][] = $extension;
-        }
-
         return $data;
-    }
-
-    /**
-     * @param array $data
-     * @param string|null $name
-     * @return null|string
-     */
-    private function findZone(string $command, array $data, string $name = null): ?string
-    {
-        if (isset($data['zone'])) {
-            return $data['zone'];
-        }
-        if (!$name && isset($data['name'])) {
-            $name = $data['name'];
-        }
-        if (!$name && isset($data['names'])) {
-            $name = reset($data['names']);
-        }
-
-        return array_pop(explode('.', $name));
     }
 }
