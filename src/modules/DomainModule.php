186fd9adeed9f246bb8028639c257d207a2722bb
Yurii Myronchuk <bladeroot@gmail.com>
2025-01-22 19:07:57 +0000

    fix renew

--- src/modules/DomainModule.php
+++ src/modules/DomainModule.php
@@ -349,7 +349,7 @@ class DomainModule extends AbstractModule
         $row = $this->_domainSetFee($row, 'renew');
 
         if (!empty($row['fee']) && floatval((string) $row['fee']) > floatval((string) $row['standart_price'])) {
-            throw new Exception($row['reason']);
+            // throw new Exception($row['reason']);
         }
 
         $curExpDate = $this->tool->getDateTime($row['expires_time']);
@@ -962,6 +962,8 @@ class DomainModule extends AbstractModule
             'curExpDate'    => $row['expires'],
             'period'        => $row['period'],
             'fee'           => $row['fee'] ?? null,
+            'category'      => $row['category'],
+            'category_name' => $row['category_name'],
         ]), array_filter([
             'domain'            => 'name',
             'expiration_date'   => 'exDate',
